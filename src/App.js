688465f24e788a80e79fe52b1ac640d530c7c6a8
Nishant Gerald <nishant.gerald@gmail.com>
2023-08-05 16:41:42 -0400

    Prints title

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ePub from 'epubjs';
 
 function App() {
+  const [selectedFile, setSelectedFile] = useState();
+
+  const handleFileUpload = (event) => {
+    console.log(event.target.files[0]);
+    setSelectedFile(event.target.files[0]);
+    handleEpub(event.target.files[0]);
+  };
+
+  const handleEpub = (file) => {
+    const reader = new FileReader();
+  
+    reader.onload = (event) => {
+      const book = ePub(event.target.result);
+      book.loaded.metadata.then((metadata) => {
+        console.log(metadata.title);
+      });
+    };
+  
+    reader.readAsArrayBuffer(file);
+  };
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>EPUB Reader</h1>
       </header>
+      <main>
+        <input type="file" accept=".epub" onChange={handleFileUpload} />
+      </main>
     </div>
   );
 }
